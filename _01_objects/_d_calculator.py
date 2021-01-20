963b48979e877fdf58ba2763f49f35c4d1e1da43
root <root@root.com>
2021-01-20 01:04:36 +0000

    calculator finished, guest book ip

--- _01_objects/_d_calculator.py
+++ _01_objects/_d_calculator.py
@@ -4,27 +4,76 @@ import tkinter as tk
 class Calculator(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.final_value = 'h'
+        self.label_text = tk.StringVar()
+        self.label_text.set('hello')
+        self.label = tk.Label(self, textvariable=self.label_text, bg='white', fg='black', font=('arial', 25), relief='solid')
 
-        self.label = tk.Label(self, title='hello')
-        self.label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
-        self.plus_button = tk.Button(self, text='Plus(+)')
-        self.plus_button.place(x=100,y=100)
-        self.minus_button = tk.Button(self, text='Minus(-)')
-        self.plus_button.place(x=200, y=100)
-        self.multiply_button = tk.Button(self, text='Multiply(x,*)')
-        self.plus_button.place(x=300, y=100)
-        self.divide_button = tk.Button(self, text='Divide(/)')
-        self.plus_button.place(x=400, y=100)
+        self.plus_button = tk.Button(self, text='Plus(+)', command=lambda: self.plus_button_pressed())
+        self.minus_button = tk.Button(self, text='Minus(-)', command=lambda: self.minus_button_pressed())
+        self.multiply_button = tk.Button(self, text='Multiply(x,*)', command=lambda: self.multiply_button_pressed())
+        self.divide_button = tk.Button(self, text='Divide(/)', command=lambda: self.divide_button_pressed())
+        self.value_one = tk.Entry(self)
+        self.value_two = tk.Entry(self)
+
+        self.label.place(relx=0.1, rely=0.7, relwidth=0.8, relheight=0.2)
+        #self.label.place(x=100, y=125)
+        self.plus_button.place(x=0, y=75)
+        self.minus_button.place(x=125, y=75)
+        self.multiply_button.place(x=250, y=75)
+        self.divide_button.place(x=375, y=75)
+        self.value_one.place(x=50, y=25)
+        self.value_two.place(x=250, y=25)
+
+    def plus_button_pressed(self):
+        text_in_text_field_one = self.value_one.get()
+        text_in_text_field_two = self.value_two.get()
+        value_one_float = float(text_in_text_field_one)
+        value_two_float = float(text_in_text_field_two)
+        final_float_value = value_one_float+value_two_float
+        print(value_one_float+value_two_float)
+        # Update the text in the label
+        self.label_text.set(final_float_value)
+
+    def minus_button_pressed(self):
+        text_in_text_field_one = self.value_one.get()
+        text_in_text_field_two = self.value_two.get()
+        value_one_float = float(text_in_text_field_one)
+        value_two_float = float(text_in_text_field_two)
+        final_float_value = value_one_float-value_two_float
+        print(value_one_float+value_two_float)
+        # Update the text in the label
+        self.label_text.set(final_float_value)
+
+    def multiply_button_pressed(self):
+        text_in_text_field_one = self.value_one.get()
+        text_in_text_field_two = self.value_two.get()
+        value_one_float = float(text_in_text_field_one)
+        value_two_float = float(text_in_text_field_two)
+        final_float_value = value_one_float * value_two_float
+        print(value_one_float * value_two_float)
+        #Update the text in the label
+        self.label_text.set(final_float_value)
+
+    def divide_button_pressed(self):
+        text_in_text_field_one = self.value_one.get()
+        text_in_text_field_two = self.value_two.get()
+        value_one_float = float(text_in_text_field_one)
+        value_two_float = float(text_in_text_field_two)
+        final_float_value = value_one_float / value_two_float
+        print(value_one_float / value_two_float)
+        # Update the text in the label
+        self.label_text.set(final_float_value)
 
 
 if __name__ == '__main__':
     app = Calculator()
-    app.title('hello')
+    app.title('Calculator')
     app.geometry('500x200')
     # Make a calculator app like the one shown in the calculator.png image
     # HINT: you'll need:
     # 1. a new class
     # 2. a StringVar variable
     # 3. Two tk.Entry widgets, four tk.Button widgets, and one tl.Label widget
-    app.maintainloop
+    app.mainloop()
     pass
--- _02_lists/_b_guest_book.py
+++ _02_lists/_b_guest_book.py
@@ -9,6 +9,18 @@ class GuestBook(tk.Tk):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.add_guest_button = tk.Button(self, text='Add guest', command=lambda: self.add_guest_button_pressed())
+        self.view_guests_button = tk.Button(self, text='View guests', command=lambda: self.view_guests_button_pressed())
+        self.ok_button = tk.Button(self, text='Ok', command=lambda: self.ok_button_pressed())
+        self.cancel_button = tk.Button(self, text='Cancel', command=lambda: self.cancel_button_pressed())
+        self.guest_name = tk.Entry(self)
+
+        self.add_guest_button.place(x=0, y=75)
+        self.view_guests_button.place(x=125, y=75)
+        #self.ok_button.place(x=250, y=75)
+        #self.cancel_button.place(x=375, y=75)
+        self.guest_name.place(x=50, y=25)
+
 
 if __name__ == '__main__':
     # 1. Make a new GuestBook
@@ -16,6 +28,6 @@ if __name__ == '__main__':
 
     # 2. Set the title
     gb.title('Guest Book')
-
+    gb.geometry('300x200')
     # 3. Run your game's mainloop
     gb.mainloop()
